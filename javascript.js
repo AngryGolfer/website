ab8ba0a9a6fa9ddbee7f5d10d959c42599a7b47d
AngryGolfer <mashhammer@protonmail.com>
2022-05-06 06:35:06 +0100

    Start adding java to website, finish fundamenta-1 and start part-2

---
+++ javascript.js
@@ -0,0 +1,35 @@
+let a = 10
+console.log(a*9)
+let b = 7 * a
+console.log(b)
+
+const MAX = 57
+let actual = 10
+percentage = actual/MAX
+
+const string = 'The revolution will not be televised.';
+console.log(string);
+const badString = string;
+console.log(badString);
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
+
+
+const myString = '123';
+const myNum = Number(myString);
+console.log(typeof myNum);
+
+const number1 = 30091996
+const string123 = Number(willthiswork)
+console.log(`${number1}, ${string123}`)
+
+
+
+
+
+
+
+
