4247ef04ae051bbba7c303fc4cf436f482d0d03a
AngryGolfer <mashhammer@protonmail.com>
2022-05-31 06:59:49 +0100

    Add javascript into blank HTML noiler plates and continued practice of java
    Tried to use functions for simplte maths uses, editing strings and spliting
     them.
    Also tried to make a function that returns a phrase all capitals and also
    another one that returns all lower case.

--- javascript.js
+++ javascript.js
@@ -20,4 +20,41 @@ console.log(dblSgl);
 
 const myString = '123';
 console.log(typeof myString);
+let mystring = "lower case string";
+mystring.toUpperCase();
+console.log(mystring);
+// 'LOWER CASE STRING'
+console.log(mystring.length)
+// 17
 
+function squared(num) {
+    return num * num;
+  }
+  
+  function cubed(num) {
+    return num * num * num;
+  }
+  
+  function factorial(num) {
+    if (num < 0) return undefined;
+    if (num == 0) return 1;
+    let x = num - 1;
+    while (x > 1) {
+      num *= x;
+      x--;
+    }
+    return num;
+  }
+  
+
+  input.addEventListener('change', () => {
+    const num = parseFloat(input.value);
+    if (isNaN(num)) {
+      para.textContent = 'You need to enter a number!';
+    } else {
+      para.textContent = `${num} squared is ${squared(num)}. `;
+      para.textContent += `${num} cubed is ${cubed(num)}. `;
+      para.textContent += `${num} factorial is ${factorial(num)}. `;
+    }
+  });
+  
